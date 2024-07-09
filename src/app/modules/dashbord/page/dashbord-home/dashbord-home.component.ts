4cf1395eaa364f5fc221b0305f656881274cc8c4
Diego Moraes dos Santos Silva <diego.moraes@grupoaguasdobrasil.com.br>
2024-07-08 21:59:19 -0300

    Grafico tela principal

--- src/app/modules/dashbord/page/dashbord-home/dashbord-home.component.ts
+++ src/app/modules/dashbord/page/dashbord-home/dashbord-home.component.ts
@@ -4,16 +4,19 @@ import { ProductsService } from 'src/app/services/products/products.service';
 import { GetAllProductsResponse } from 'src/app/Models/interfaces/products/response/GetAllProductsResponse';
 import { ProductsDataTransferService } from 'src/app/shared/services/products/products-data-transfer.service';
 import { Subject, takeUntil } from 'rxjs';
+import { ChartData, ChartOptions, Colors } from 'chart.js';
 
 @Component({
   selector: 'app-dashbord-home',
   templateUrl: './dashbord-home.component.html',
   styleUrls: [],
 })
-export class DashbordHomeComponent implements OnInit , OnDestroy {
+export class DashbordHomeComponent implements OnInit, OnDestroy {
   private destroy$ = new Subject<void>();
-
   public productsList: Array<GetAllProductsResponse> = [];
+
+  public productsChartDatas!: ChartData;
+  public productsChartOptions!: ChartOptions;
 
   constructor(
     private productsService: ProductsService,
@@ -26,28 +29,87 @@ export class DashbordHomeComponent implements OnInit , OnDestroy {
   }
 
   getProductsDatas(): void {
-    this.productsService.getAllProducts()
-    .pipe(
-      takeUntil(this.destroy$)
-    )
-    .subscribe({
-      next: (response) => {
-        if (response.length > 0) {
-          this.productsList = response;
-          this.produtsDtService.setProductsDatas(this.productsList);
-        }
-      },
-      error: (err) => {
-        console.log(err);
-        this.messageService.add({
-          severity: 'error',
-          summary: 'Erro',
-          detail: 'Erro ao Buscar Produtos',
-          life: 2500,
-        });
-      },
-    });
+    this.productsService
+      .getAllProducts()
+      .pipe(takeUntil(this.destroy$))
+      .subscribe({
+        next: (response) => {
+          if (response.length > 0) {
+            this.productsList = response;
+            this.produtsDtService.setProductsDatas(this.productsList);
+            this.setProductsChartConfig();
+          }
+        },
+        error: (err) => {
+          console.log(err);
+          this.messageService.add({
+            severity: 'error',
+            summary: 'Erro',
+            detail: 'Erro ao Buscar Produtos',
+            life: 2500,
+          });
+        },
+      });
   }
+
+  setProductsChartConfig(): void {
+    if (this.productsList.length > 0) {
+      const documentStyle = getComputedStyle(document.documentElement);
+      const textColor = documentStyle.getPropertyValue('--text-color');
+      const textColorSecondary = documentStyle.getPropertyValue(
+        '--text-color-secondary'
+      );
+      const surfaceBorder = documentStyle.getPropertyValue('--surface-border');
+
+      this.productsChartDatas = {
+        labels: this.productsList.map((element) => element?.name),
+        datasets: [
+          {
+            label: 'Quantidade',
+            backgroundColor: documentStyle.getPropertyValue('--indigo-400'),
+            borderColor: documentStyle.getPropertyValue('--indigo-400'),
+            hoverBackgroundColor:
+              documentStyle.getPropertyValue('--indigo-500'),
+            data: this.productsList.map((element) => element?.amount),
+          },
+        ],
+      };
+      this.productsChartOptions = {
+        maintainAspectRatio: false,
+        aspectRatio: 0.8,
+        plugins: {
+          legend: {
+            labels: {
+              color: textColor,
+            },
+          },
+        },
+
+        scales: {
+          x: {
+            ticks: {
+              color: textColorSecondary,
+              font: {
+                weight: 500,
+              },
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+          y: {
+            ticks: {
+              color: textColorSecondary,
+            },
+            grid: {
+              color: surfaceBorder,
+            },
+          },
+        },
+      };
+    }
+  }
+
   ngOnDestroy(): void {
     this.destroy$.next();
     this.destroy$.complete();
